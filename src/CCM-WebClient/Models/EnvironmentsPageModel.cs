f031c123a029af066fa86165a6bc8e78a06f8f78
Felipe Quintella <felipe@quintella.com>
2020-05-06 16:28:24 -0300

    INC

--- src/CCM-WebClient/Models/EnvironmentsPageModel.cs
+++ src/CCM-WebClient/Models/EnvironmentsPageModel.cs
@@ -1,7 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Blazorise.DataGrid;
+using Domain;
+using Environment = Domain.Environment;
+
 namespace CCM_WebClient.Models
 {
-    public class EnvironmentsPageModel
+    public class EnvironmentsPageModel: BaseModel
     {
+        protected List<Environment> Environments { get; set; }
         
+        protected Environment SelectedEnvironment { get; set; }
+        
+        protected override void OnInitialized()
+        {
+            Environments = new List<Environment>();
+        }
+
+        public Action<CancellableRowChange<Environment, Dictionary<string, object>>> InsertingAction { get; set; }
+
+
+        public EnvironmentsPageModel()
+        {
+            InsertingAction = OnInserting;
+        }
+        
+        protected void OnInserting(CancellableRowChange<Environment, Dictionary<string, object>> crc)
+        {
+
+        }
+        
+        protected void OnRowInserted(SavedRowItem<Environment, Dictionary<string, object>> e)
+        {
+            // The user is new 
+
+        }
+        
+        protected void OnRemoving(CancellableRowChange<Environment> crc)
+        {
+
+        }
+        
+        protected void OnRowUpdated( SavedRowItem<Environment, Dictionary<string, object>> e )
+        {
+
+        }
     }
 }
